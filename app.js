458866907a8edb1cd59246ad075bfc65f4ae79d9
Eric Olaveson <olavesoneric@gmail.com>
2019-03-27 21:18:38 -0500

    done - final

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ function setDate() {
   const now = new Date();
 
   const seconds = now.getSeconds();
-  const secondsDegrees = (seconds / 60) * 360 + 90;
+  let s = 0;
+  s += seconds;
+  const secondsDegrees = (s / 60) * 360 + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
   const minutes = now.getMinutes();
